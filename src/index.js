a9490c257c61d2186edd0fa77a8e9cdf4578ec2d
GordonNY <gordonsdev@gmail.com>
2016-09-26 13:50:45 -0400

    render root in index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,9 @@ import './styles/main.css';
 /* Require configureStore and Root here */
 import { configureStore } from './store/configureStore';
 import { Root } from './containers/Root';
+
+const store = configureStore();
+ReactDOM.render(
+  <Root store={store} />,
+  document.getElementById('root')
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
-/* Import children reducers here */
 
+/* Import children reducers here */
+import childReducer1 from './childReducer1'; // Fake 
 
 const rootReducer = combineReducers({
   childReducer1: childReducer1,
