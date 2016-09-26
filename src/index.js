9eec74757f5a3ef50222bcfe67bc58f5762c22dd
GordonNY <gordonsdev@gmail.com>
2016-09-26 13:29:03 -0400

    add index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+/* Import stylesheet here */
+import './styles/main.css';
+
+
+/* Require configureStore and Root here */
+import { configureStore } from './store/configureStore';
+import { Root } from './containers/Root';
