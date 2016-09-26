8dac868d85d736248c6b2b0edd74d7575a48c272
GordonNY <gordonsdev@gmail.com>
2016-09-26 13:35:42 -0400

    add dev&prod store and rootReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+/* Import children reducers here */
+
+
+const rootReducer = combineReducers({
+  childReducer1: childReducer1,
+});
+
+export default rootReducer;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,1 +1,10 @@
 /* Uniqe Store */
+let loadedStore = null;
+
+if (process.env.NODE_ENV === 'production') {
+  loadedStore = require('./configureStore.prod');
+} else {
+  loadedStore = require('./configureStore.dev');
+}
+
+export const configureStore = loadedStore;
