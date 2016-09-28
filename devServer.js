c9ec50021e696f35afa4792ffbd1740a33829e91
Gordon Gao <yuangao0317@gmail.com>
2016-09-28 09:15:12 -0400

    fix devServer.js

--- devServer.js
+++ devServer.js
@@ -1,17 +1,25 @@
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const config = require('./webpack.config.dev');
+
+
 
 const app = express();
-const compiler = webpack(config);
+app.use('/static', express.static(path.join(__dirname, './static')));
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
 
-app.use(require('webpack-hot-middleware')(compiler));
+if (process.env.NODE_ENV !== 'production') {
+	const webpack = require('webpack');
+	const config = require('./webpack.config.dev');
+	const compiler = webpack(config);
+
+	app.use(require('webpack-dev-middleware')(compiler, {
+	  noInfo: true,
+	  publicPath: config.output.publicPath
+	}));
+
+	app.use(require('webpack-hot-middleware')(compiler));
+}
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
