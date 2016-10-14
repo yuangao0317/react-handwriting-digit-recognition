3852948a590546eb918b3468ca3baea7df21106a
Gordon Gao <yuangao0317@gmail.com>
2016-10-14 13:16:16 -0400

    add blog links

--- src/components/Details.js
+++ src/components/Details.js
@@ -17,9 +17,15 @@ export default class Details extends Component {
       					<h4>Data Set</h4>
       					<p>I used the <a href="http://yann.lecun.com/exdb/mnist/">MNIST database of handwritten digits</a>. It is a good database for people who want to try learning techniques and pattern recognition methods on real-world data while spending minimal efforts on preprocessing and formatting.</p>
       				</li>
+      				<li>
+      					<h4>Implement Detail</h4>
+      					<p>For the Neural Network Implement details, checkout my blogs:</p>
+      					<ul>
+      						<li><a href="https://github.com/YuanGao0317/A-Machine-Learning-Blog-Series/blob/master/Blog/5%20Neural%20Networks.pdf">Neural Network</a></li>
+      						<li><a href="https://github.com/YuanGao0317/A-Machine-Learning-Blog-Series/blob/master/Blog/6%20Backpropagation%20Algorithm.pdf">Backpropagation Algorithm</a></li>
+      					</ul>
+      				</li>
       			</ul>
-      			
-      			
       		</div>
       	</div>
       </div>
